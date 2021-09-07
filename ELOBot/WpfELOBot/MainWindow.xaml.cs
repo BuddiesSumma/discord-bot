10857a722573826e6383fa887c4c69ffc6723b60
PS197989 <ps197989@edu.summacollege.nl>
2021-09-07 14:40:56 +0200

    Commentaar opgeschoond

--- ELOBot/WpfELOBot/MainWindow.xaml.cs
+++ ELOBot/WpfELOBot/MainWindow.xaml.cs
@@ -49,8 +49,6 @@ namespace WpfELOBot
             _client = new DiscordSocketClient();
             _client.MessageReceived += CommandHandler;
 
-            //  You can assign your bot token to a string, and pass that in to connect.
-            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
             var token = File.ReadAllText("token.txt");
 
             await _client.LoginAsync(TokenType.Bot, token);
